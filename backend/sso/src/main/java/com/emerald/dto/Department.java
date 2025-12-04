e462fd6b2d2a878fe548473a79b4d71ef56f17c0
scanalesR <scanales@rocketsoftware.com>
2025-12-04 16:41:30 -0500

    Begin implementing DTOs for Department, Login, and User

--- backend/sso/src/main/java/com/emerald/dto/Department.java
+++ backend/sso/src/main/java/com/emerald/dto/Department.java
@@ -3,4 +3,9 @@ package com.emerald.dto;
 public class Department {
     private int id;
     private String name;
+
+    public Department(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 }
