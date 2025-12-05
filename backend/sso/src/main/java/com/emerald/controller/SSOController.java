1339699668de242d8f8abdb1964dc5f5e8827d35
jbirchfield-rocket <jbirchfield@rocketsoftware.com>
2025-12-05 12:55:26 -0500

    Runs on port 6767 and added templates for some endpoints

--- backend/sso/src/main/java/com/emerald/controller/SSOController.java
+++ backend/sso/src/main/java/com/emerald/controller/SSOController.java
@@ -6,7 +6,10 @@
 package com.emerald.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.HttpStatus;
@@ -26,6 +29,38 @@ public class SSOController {
         return ResponseEntity.status(HttpStatus.OK)
                              .contentType(MediaType.APPLICATION_JSON)
                              .body("{\"message\": \"Hello\"}");
+    }
+
+    @GetMapping("/viewaccount")
+    public ResponseEntity<String> viewAccount(){
+
+        return ResponseEntity.status(HttpStatus.OK)
+                             .contentType(MediaType.APPLICATION_JSON)
+                             .body("{\"message\": \"This is your account\"}");
+    }
+
+    @PostMapping("/createaccount")
+    public ResponseEntity<String> createAccount(){
+
+        return ResponseEntity.status(HttpStatus.OK)
+                             .contentType(MediaType.APPLICATION_JSON)
+                             .body("{\"message\": \"Account created.\"}");
+    }
+
+    @DeleteMapping("/deleteaccount")
+    public ResponseEntity<String> deleteAccount(){
+
+        return ResponseEntity.status(HttpStatus.OK)
+                             .contentType(MediaType.APPLICATION_JSON)
+                             .body("{\"message\": \"Account deleted\"}");
+    }
+
+    @PutMapping("/changepassword")
+    public ResponseEntity<String> changePassword(){
+
+        return ResponseEntity.status(HttpStatus.OK)
+                             .contentType(MediaType.APPLICATION_JSON)
+                             .body("{\"message\": \"Password Changed\"}");
     }
 
 }
