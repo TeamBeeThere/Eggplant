ff2fa290f13ca0d7165602edc3adb92ca3c575e2
jbirchfield-rocket <jbirchfield@rocketsoftware.com>
2025-12-05 11:41:22 -0600

    Added com.emerald to component scan

--- backend/sso/src/main/java/com/emerald/sso/SsoApplication.java
+++ backend/sso/src/main/java/com/emerald/sso/SsoApplication.java
@@ -2,11 +2,13 @@ package com.emerald.sso;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 
 import com.emerald.config.SpringConfig;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.emerald")
 @Import(SpringConfig.class)
 public class SsoApplication {
 
