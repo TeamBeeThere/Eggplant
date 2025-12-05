15fef3272f0d9de8a6a3bd923f6adf5197951744
jbirchfield-rocket <jbirchfield@rocketsoftware.com>
2025-12-05 17:45:49 -0500

    Added comments for ideas of how to implement endpoints

--- backend/sso/src/main/java/com/emerald/controller/SSOController.java
+++ backend/sso/src/main/java/com/emerald/controller/SSOController.java
@@ -12,6 +12,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.emerald.repository.UserRepository;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
@@ -23,6 +26,13 @@ import org.springframework.http.MediaType;
 @RequestMapping("/sso")
 public class SSOController {
 
+    //Once repository is done
+        // private final UserRepository repository;
+
+        // SSOController(UserRepository repository){
+        //     this.repository = repository;
+        // }
+    
     @GetMapping(("/hello"))
     public ResponseEntity<String> greetings(){
         
@@ -31,8 +41,10 @@ public class SSOController {
                              .body("{\"message\": \"Hello\"}");
     }
 
-    @GetMapping("/viewaccount")
-    public ResponseEntity<String> viewAccount(){
+    @GetMapping("/viewaccount" /* /viewaccount/{id} */)
+    public ResponseEntity<String> viewAccount(/*@PathVariable Long id */){
+
+        // repository.getAccountById
 
         return ResponseEntity.status(HttpStatus.OK)
                              .contentType(MediaType.APPLICATION_JSON)
@@ -40,15 +52,21 @@ public class SSOController {
     }
 
     @PostMapping("/createaccount")
-    public ResponseEntity<String> createAccount(){
+    public ResponseEntity<String> createAccount( /* @RequestBody Employee newEmployee */){
+
+        //repository.save(newEmployee)
 
         return ResponseEntity.status(HttpStatus.OK)
                              .contentType(MediaType.APPLICATION_JSON)
                              .body("{\"message\": \"Account created.\"}");
     }
 
-    @DeleteMapping("/deleteaccount")
-    public ResponseEntity<String> deleteAccount(){
+    @DeleteMapping("/deleteaccount" /* "/deleteaccount/{id}" */)
+    public ResponseEntity<String> deleteAccount( /* @RequestBody Employee updatedEmployee, @PathVariable Long id */){
+
+        /*
+        repository.deleteById(id)
+        */
 
         return ResponseEntity.status(HttpStatus.OK)
                              .contentType(MediaType.APPLICATION_JSON)
@@ -57,6 +75,14 @@ public class SSOController {
 
     @PutMapping("/changepassword")
     public ResponseEntity<String> changePassword(){
+
+        /*
+        repository.findById(id).map(employee -> { 
+            employee.setName(newEmployee.getName());
+            employee.setRole(newEmployee.getRole());
+            return repository.save(employee)
+         })
+        */
 
         return ResponseEntity.status(HttpStatus.OK)
                              .contentType(MediaType.APPLICATION_JSON)
